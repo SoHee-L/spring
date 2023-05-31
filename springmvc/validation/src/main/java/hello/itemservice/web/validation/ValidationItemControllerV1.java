110e8c62ec30174141604e64efa1a0f84558f7f6
SoHee-L <lsohee12@gmail.com>
2023-05-31 16:04:08 +0900

    [제목] springmvc - Validation / 검증직접처리 - 개발까지 수강

--- springmvc/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ springmvc/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,13 +3,18 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -30,15 +35,49 @@ public class ValidationItemControllerV1 {
         model.addAttribute("item", item);
         return "validation/v1/item";
     }
-
+    //form 을 보여주는 부분
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
         return "validation/v1/addForm";
     }
-
+    //실제 저장 부분
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+        //검증에 실패하면 뭐가 실패했는지에 대한 정보를 다시 담아서 내리는 로직 필요.
+        //검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+
+        //검증 로직
+        //itemName 에 글자가 없으면
+        if(!StringUtils.hasText(item.getItemName())){
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000){
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if(item.getQuantity() == null || item.getQuantity() >= 9999){
+            errors.put("quantity", " 수량은 최대 9,999까지 허용합니다.");
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            //가격과 수량을 곱했을 때 10000원 이상이여야 하는 검증 로직.
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " +resultPrice);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        //에러가 있으면이라는 의미지만 읽기 어려움
+        if(!errors.isEmpty()) { //=> 부정의 부정어
+            log.info("error = {}", errors);
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
+        //성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
