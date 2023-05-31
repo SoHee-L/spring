21c645a41c33b4d6bf846aabdc7b2e84864a5397
SoHee-L <shlee@ubi-plus.com>
2023-05-31 13:55:55 +0900

    [제목] springmvc - 메시지, 국제화 / 수강 완료

--- springmvc/message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ springmvc/message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +12,14 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+//	//스프링 부트가 자동으로 해주는 부분이지만 수동 예시
+//	@Bean
+//	public MessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new
+//				ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 
 }
